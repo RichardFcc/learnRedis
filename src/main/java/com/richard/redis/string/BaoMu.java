9bc30e66efed8bb2ac19b136c7af3ecdaa1e0c0c
Mr.F <nikfu@foxmail.com>
2017-11-22 15:37:00 +0800

    增加String数据结构的注释

---
+++ src/main/java/com/richard/redis/string/BaoMu.java
@@ -0,0 +1,32 @@
+package com.richard.redis.string;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Describe: 报幕线程，从redis中获取比武场次，然后打印出来
+ * Author:   niklaus
+ * Domain:   www.niklaus.com
+ * Data:     2016/1/6.
+ */
+public class BaoMu implements Runnable {
+    private Jedis jedis;
+    private String redisKey;
+
+    public BaoMu(String redisKey) {
+        this.redisKey = redisKey;
+    }
+
+    public void run() {
+        jedis = new Jedis("127.0.0.1",6379);
+        while (true) {
+            try {
+                Thread.sleep(1000);
+                System.out.println("===================当前总共比武次数为：" + jedis.get(redisKey));
+            } catch (Exception e) {
+                System.out.println("擂台被损坏..."+e);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/richard/redis/string/Person.java
@@ -0,0 +1,47 @@
+package com.richard.redis.string;
+
+import java.io.Serializable;
+
+/**
+ * Describe: 用户的javaBean
+ * Author:   niklaus
+ * Domain:   www.niklaus.com
+ * Data:     2016/1/6.
+ */
+public class Person implements Serializable{
+    private static final long serialVersionUID = -9012113097419111583L;
+    private String name;//姓名
+    private int age;//年龄
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
